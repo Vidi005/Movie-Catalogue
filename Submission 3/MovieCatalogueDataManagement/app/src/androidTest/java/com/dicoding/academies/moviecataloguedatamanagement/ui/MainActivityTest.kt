534acbafd46228ad5b57213ab97799fbea4efb36
Vidi Fitriansyah Hidarlan <55016463+Vidi005@users.noreply.github.com>
2021-03-04 11:58:31 +0700

    Revision

--- Submission 3/MovieCatalogueDataManagement/app/src/androidTest/java/com/dicoding/academies/moviecataloguedatamanagement/ui/MainActivityTest.kt
+++ Submission 3/MovieCatalogueDataManagement/app/src/androidTest/java/com/dicoding/academies/moviecataloguedatamanagement/ui/MainActivityTest.kt
@@ -9,9 +9,6 @@ import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.contrib.RecyclerViewActions
 import androidx.test.espresso.matcher.ViewMatchers.*
 import com.dicoding.academies.moviecataloguedatamanagement.R
-import com.dicoding.academies.moviecataloguedatamanagement.utils.DataDummy.generateDummyDetailMovie
-import com.dicoding.academies.moviecataloguedatamanagement.utils.DataDummy.generateDummyDetailTVShow
-import com.dicoding.academies.moviecataloguedatamanagement.utils.DataDummy.generateDummyMovies
 import com.dicoding.academies.moviecataloguedatamanagement.utils.DataDummy.generateDummyTVShows
 import com.dicoding.academies.moviecataloguedatamanagement.utils.EspressoIdlingResource.espressoIdlingResource
 import org.junit.After
@@ -20,12 +17,7 @@ import org.junit.Test
 
 class MainActivityTest {
 
-    private val dummyMovies = generateDummyMovies()
-    private val movieId = dummyMovies[0].id
-    private val dummyDetailMovie = generateDummyDetailMovie(movieId as Int)
     private val dummyTvShows = generateDummyTVShows()
-    private val tvId = dummyTvShows[2].id
-    private val dummyDetailTvShow = generateDummyDetailTVShow(tvId as Int)
 
     @Before
     fun setUp() {
@@ -54,29 +46,15 @@ class MainActivityTest {
         onView(withId(R.id.sv_detail_movie)).check(matches(isDisplayed()))
         onView(withId(R.id.riv_item_poster_received)).check(matches(isDisplayed()))
         onView(withId(R.id.tv_item_movie_title_received)).check(matches(isDisplayed()))
-        onView(withId(R.id.tv_item_movie_title_received))
-            .check(matches(withText(dummyDetailMovie[0].title)))
         onView(withId(R.id.tv_item_year_received)).check(matches(isDisplayed()))
-        onView(withId(R.id.tv_item_year_received))
-            .check(matches(withText("${dummyDetailMovie[0].releaseDate?.take(4)}")))
         onView(withId(R.id.tv_item_release_date_received)).check(matches(isDisplayed()))
-        onView(withId(R.id.tv_item_release_date_received))
-            .check(matches(withText(dummyDetailMovie[0].releaseDate)))
         onView(withId(R.id.tv_item_genres_received)).check(matches(isDisplayed()))
-        onView(withId(R.id.tv_item_genres_received))
-            .check(matches(withText(dummyDetailMovie[0].genres)))
         onView(withId(R.id.tv_item_popularity_received)).check(matches(isDisplayed()))
         onView(withId(R.id.tv_item_score_received)).check(matches(isDisplayed()))
         onView(withId(R.id.tv_item_tagLine_received)).check(matches(isDisplayed()))
-        onView(withId(R.id.tv_item_tagLine_received))
-            .check(matches(withText(dummyDetailMovie[0].tagLine)))
         onView(withId(R.id.tv_item_overview_received)).check(matches(isDisplayed()))
-        onView(withId(R.id.tv_item_overview_received))
-            .check(matches(withText(dummyDetailMovie[0].overview)))
         onView(withId(R.id.sv_detail_movie)).perform(swipeUp())
         onView(withId(R.id.tv_item_production_companies_received)).check(matches(isDisplayed()))
-        onView(withId(R.id.tv_item_production_companies_received))
-            .check(matches(withText(dummyDetailMovie[0].productionCompanies)))
     }
 
     @Test
@@ -105,30 +83,18 @@ class MainActivityTest {
                 .scrollToPosition<RecyclerView.ViewHolder>(dummyTvShows.size))
         onView(withId(R.id.rv_tv_show))
             .perform(RecyclerViewActions
-                .actionOnItemAtPosition<RecyclerView.ViewHolder>(2, click()))
+                .actionOnItemAtPosition<RecyclerView.ViewHolder>(dummyTvShows.size, click()))
         onView(withId(R.id.sv_detail_tv_show)).check(matches(isDisplayed()))
         onView(withId(R.id.riv_item_poster_received)).check(matches(isDisplayed()))
         onView(withId(R.id.tv_item_tv_show_name_received)).check(matches(isDisplayed()))
-        onView(withId(R.id.tv_item_tv_show_name_received))
-            .check(matches(withText(dummyDetailTvShow[2].name)))
         onView(withId(R.id.tv_item_first_air_date_received)).check(matches(isDisplayed()))
-        onView(withId(R.id.tv_item_first_air_date_received))
-            .check(matches(withText(dummyDetailTvShow[2].firstAirDate.toString())))
         onView(withId(R.id.tv_item_genres_received)).check(matches(isDisplayed()))
-        onView(withId(R.id.tv_item_genres_received))
-            .check(matches(withText(dummyDetailTvShow[2].genres)))
         onView(withId(R.id.tv_item_popularity_received)).check(matches(isDisplayed()))
         onView(withId(R.id.tv_item_score_received)).check(matches(isDisplayed()))
         onView(withId(R.id.tv_item_tagLine_received)).check(matches(isDisplayed()))
-        onView(withId(R.id.tv_item_tagLine_received))
-            .check(matches(withText(dummyDetailTvShow[2].tagLine)))
         onView(withId(R.id.tv_item_overview_received)).check(matches(isDisplayed()))
-        onView(withId(R.id.tv_item_overview_received))
-            .check(matches(withText(dummyDetailTvShow[2].overview)))
         onView(withId(R.id.sv_detail_tv_show)).perform(swipeUp())
         onView(withId(R.id.tv_item_production_companies_received)).check(matches(isDisplayed()))
-        onView(withId(R.id.tv_item_production_companies_received))
-            .check(matches(withText(dummyDetailTvShow[2].productionCompanies)))
     }
 
     @Test
@@ -146,6 +112,8 @@ class MainActivityTest {
     fun loadFavoriteMovies() {
         onView(withId(R.id.rv_movie))
             .perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(0, click()))
+        onView(withId(R.id.app_bar_favorite)).check(matches(isDisplayed()))
+        onView(withId(R.id.app_bar_favorite)).check(matches(isClickable()))
         onView(withId(R.id.app_bar_favorite)).perform(click())
         onView(isRoot()).perform(pressBack())
         onView(withId(R.id.navigation_favorites)).check(matches(isDisplayed()))
@@ -164,6 +132,8 @@ class MainActivityTest {
         onView(withId(R.id.tv_item_overview_received)).check(matches(isDisplayed()))
         onView(withId(R.id.sv_detail_movie)).perform(swipeUp())
         onView(withId(R.id.tv_item_production_companies_received)).check(matches(isDisplayed()))
+        onView(withId(R.id.app_bar_favorite)).perform(click())
+        onView(isRoot()).perform(pressBack())
     }
 
     @Test
@@ -190,6 +160,8 @@ class MainActivityTest {
         onView(withId(R.id.tv_item_overview_received)).check(matches(isDisplayed()))
         onView(withId(R.id.sv_detail_tv_show)).perform(swipeUp())
         onView(withId(R.id.tv_item_production_companies_received)).check(matches(isDisplayed()))
+        onView(withId(R.id.app_bar_favorite)).perform(click())
+        onView(isRoot()).perform(pressBack())
     }
 
     @Test
