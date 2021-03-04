5759081df90e205d95da0916f9aa15c1625358b9
Vidi Fitriansyah Hidarlan <55016463+Vidi005@users.noreply.github.com>
2021-03-04 16:10:32 +0700

    Revision

--- Submission 3/MovieCatalogueDataManagement/app/src/main/java/com/dicoding/academies/moviecataloguedatamanagement/utils/AppExecutors.kt
+++ Submission 3/MovieCatalogueDataManagement/app/src/main/java/com/dicoding/academies/moviecataloguedatamanagement/utils/AppExecutors.kt
@@ -7,24 +7,16 @@ import java.util.concurrent.Executor
 import java.util.concurrent.Executors
 
 class AppExecutors @VisibleForTesting constructor(
-    private val diskIO: Executor,
-    private val networkIO: Executor,
-    private val mainThread: Executor
+        private val diskIO: Executor,
+        private val mainThread: Executor
 ) {
-
-    companion object {
-        private const val THREAD_COUNT = 3
-    }
 
     constructor() : this(
         Executors.newSingleThreadExecutor(),
-        Executors.newFixedThreadPool(THREAD_COUNT),
         MainThreadExecutor()
     )
 
     fun diskIO(): Executor = diskIO
-
-    fun networkIO(): Executor = networkIO
 
     fun mainThread(): Executor = mainThread
 
